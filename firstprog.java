902f369812e7f2f8b1e57e3b9d88085efdfdff33
tanya1998 <tanya16108@iiitd.ac.in>
2016-12-28 21:20:59 +0530

    Add files via upload

---
+++ firstprog.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+	public class firstprog {
+			public static void main( String[] args ) {
+			public static int u =7;
+			Scanner keyboard= new Scanner (System.in);
+			int a,b,sub;
+			System.out.println(u);
+			u=9;
+			System.out.println(u);
+			System.out.println ("I am determined to learn java");
+			System.out.println("Bravo what's your name");
+			sub=keyboard.nextInt();
+			System.out.println(sub);
+		}
+	}
